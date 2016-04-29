1cf4649e5a7c941e16d3ce123ebe10eef3793a46
Michael B <michaelbor2015@gmail.com>
2016-04-28 20:50:34 -0700

    before merging

---
+++ app/src/main/java/com/mikhail/sportsnewshistoryrecords/api/keys/NytKeys.java
@@ -0,0 +1,11 @@
+package com.mikhail.sportsnewshistoryrecords.api.keys;
+
+/**
+ * Created by Mikhail on 4/27/16.
+ */
+public class NytKeys {
+
+    public static final String newsWireKey = "40c646e5f344c31e3c2c5d6a417cd45a:9:75031049";
+    public static final String nyTimesFullSearchQueryKey = "c51532649e15530848d0c09150ccac2f:19:75030444";
+
+}
