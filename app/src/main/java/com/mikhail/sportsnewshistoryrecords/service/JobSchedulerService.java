7039fe83cb1ef21ff2098e1799ed4a3389aab7b7
Michael B <michaelbor2015@gmail.com>
2016-05-11 15:17:13 -0700

    new thread for job finish for service

--- app/src/main/java/com/mikhail/sportsnewshistoryrecords/service/JobSchedulerService.java
+++ app/src/main/java/com/mikhail/sportsnewshistoryrecords/service/JobSchedulerService.java
@@ -66,13 +66,12 @@ public class JobSchedulerService extends JobService {
     boolean soccerCheck = false;
     boolean[] booleenArray;
     NotificationManager notificationManager;
-    JobParameters params;
 
 
     @Override
     public boolean onStartJob(JobParameters params) {
         Log.d("Notifications", "onStartJob sent");
-        this.params = params;
+        final JobParameters jobParameters = params;
         setBooleanArray();
         context = getApplicationContext();
 
@@ -81,6 +80,19 @@ public class JobSchedulerService extends JobService {
         updateBooleans();
 
         setApiCall();
+
+        Thread closeJobScheduler = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(60000);
+                    jobFinished(jobParameters, false);
+                } catch (Exception e) {
+                    e.getLocalizedMessage();
+                }
+            }
+        });
+        closeJobScheduler.run();
 
         return true;
 
@@ -150,7 +162,6 @@ public class JobSchedulerService extends JobService {
         notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, mBuilder.build());
 
-        jobFinished(params, false);
 
 
     }
